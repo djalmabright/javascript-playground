b777d4c23086d064ad4afd6d25baa0f40ca4fba7
Djalma  Bina <djbina2000@yahoo.com.br>
2017-12-11 16:48:05 -0200

    Update and rename scope  model to scope_model.js

---
+++ scope_model.js
@@ -0,0 +1,36 @@
+//scope  models
+var Module = (function () {
+  return {
+    myMethod: function () {
+      console.log('myMethod has been called.');
+    }
+  };
+})();
+
+var name = 'Todd';
+var scope1 = function () {
+  // name is available here
+  var scope2 = function () {
+    // name is available here too
+    var scope3 = function () {
+      // name is also available here!
+    };
+  };
+};
+ 
+// with  "this" 
+var myFunction = function () {
+  console.log(this); // this = global, [object Window]
+};
+myFunction();
+
+var myObject = {};
+myObject.myMethod = function () {
+  console.log(this); // this = Object { myObject }
+};
+
+var nav = document.querySelector('.nav'); // <nav class="nav">
+var toggleNav = function () {
+  console.log(this); // this = <nav> element
+};
+nav.addEventListener('click', toggleNav, false);
