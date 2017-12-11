266b4db140abe7def42756cb1192d6160d91c8fb
Djalma  Bina <djbina2000@yahoo.com.br>
2017-12-11 17:25:49 -0200

    Create nodejs-core-modules.js

---
+++ nodejs-core-modules.js
@@ -0,0 +1,102 @@
+// Assert ------------------------------
+const assert = require('assert');
+assert.equal(1, 1);
+// OK, 1 == 1
+assert.equal(1, '1');
+// OK, 1 == '1'
+
+assert.equal(1, 2);
+// AssertionError: 1 == 2
+assert.equal({ a: { b: 1 } }, { a: { b: 1 } });
+//AssertionError: { a: { b: 1 } } == { a: { b: 1 } }
+
+// Async Hooks   
+const async_hooks = require('async_hooks');
+
+// Return the ID of the current execution context.
+const eid = async_hooks.executionAsyncId();
+
+// Return the ID of the handle responsible for triggering the callback of the
+// current execution scope to call.
+const tid = async_hooks.triggerAsyncId();
+
+// Create a new AsyncHook instance. All of these callbacks are optional.
+const asyncHook =
+    async_hooks.createHook({ init, before, after, destroy, promiseResolve });
+
+// Allow callbacks of this AsyncHook instance to call. This is not an implicit
+// action after running the constructor, and must be explicitly run to begin
+// executing callbacks.
+asyncHook.enable();
+
+// Disable listening for new asynchronous events.
+asyncHook.disable();
+
+//
+// The following are the callbacks that can be passed to createHook().
+//
+
+// init is called during object construction. The resource may not have
+// completed construction when this callback runs, therefore all fields of the
+// resource referenced by "asyncId" may not have been populated.
+function init(asyncId, type, triggerAsyncId, resource) { }
+
+// before is called just before the resource's callback is called. It can be
+// called 0-N times for handles (e.g. TCPWrap), and will be called exactly 1
+// time for requests (e.g. FSReqWrap).
+function before(asyncId) { }
+
+// after is called just after the resource's callback has finished.
+function after(asyncId) { }
+
+// destroy is called when an AsyncWrap instance is destroyed.
+function destroy(asyncId) { }
+
+// promiseResolve is called only for promise resources, when the
+// `resolve` function passed to the `Promise` constructor is invoked
+// (either directly or through other means of resolving a promise).
+function promiseResolve(asyncId) { }
+
+//  Buffer
+C++ Addons
+C/C++ Addons - N-API
+Child Processes
+Cluster
+Command Line Options
+Console
+Crypto
+Debugger
+DNS
+Domain
+ECMAScript Modules
+Errors
+Events
+File System
+Globals
+HTTP
+HTTP/2
+HTTPS
+Inspector
+Internationalization
+Modules
+Net
+OS
+Path
+Performance Hooks
+Process
+Punycode
+Query Strings
+Readline
+REPL
+Stream
+String Decoder
+Timers
+TLS/SSL
+Tracing
+TTY
+UDP/Datagram
+URL
+Utilities
+V8
+VM
+ZLIB
